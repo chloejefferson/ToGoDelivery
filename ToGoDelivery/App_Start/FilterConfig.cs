693c32ebd1ffc91cbee563926da495f9ca093b22
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-04-15 19:46:44 -0400

    Add project files.

---
+++ ToGoDelivery/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ToGoDelivery
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ToGoDelivery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ToGoDelivery.MvcApplication" Language="C#" %>
