0bda95befec5eb6f90e9d44658cbdf848e441da6
chloejefferson <73564563+chloejefferson@users.noreply.github.com>
2021-05-04 18:39:20 -0400

    Reworking product views/service/controllers now functional

---
+++ ToGoDelivery.Data/Migrations/202105042229017_InitialMigration.cs
@@ -0,0 +1,184 @@
+namespace ToGoDelivery.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.OrderProduct",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        ProductCount = c.Int(nullable: false),
+                        IsActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.OrderId, t.ProductId })
+                .ForeignKey("dbo.Order", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Product", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Order",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        CustomerId = c.String(nullable: false, maxLength: 128),
+                        DateCreated = c.DateTime(nullable: false),
+                        IsActive = c.Boolean(nullable: false),
+                        FinalTotalCost = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        IsFavorite = c.Boolean(nullable: false),
+                        IsPrepared = c.Boolean(nullable: false),
+                        IsFinalized = c.Boolean(nullable: false),
+                        DateFinalized = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.ApplicationUser", t => t.CustomerId, cascadeDelete: true)
+                .Index(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.ApplicationUser",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUser", t => t.ApplicationUser_Id)
+                .ForeignKey("dbo.IdentityRole", t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id);
+            
+            CreateTable(
+                "dbo.Product",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Inventory = c.Int(nullable: false),
+                        Cost = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        IsActive = c.Boolean(nullable: false),
+                        CreatedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.OrderService",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false),
+                        ServiceId = c.Int(nullable: false),
+                        Cost = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => new { t.OrderId, t.ServiceId })
+                .ForeignKey("dbo.Order", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Service", t => t.ServiceId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ServiceId);
+            
+            CreateTable(
+                "dbo.Service",
+                c => new
+                    {
+                        ServiceId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Cost = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ServiceId);
+            
+            CreateTable(
+                "dbo.IdentityRole",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.IdentityUserRole", "IdentityRole_Id", "dbo.IdentityRole");
+            DropForeignKey("dbo.OrderService", "ServiceId", "dbo.Service");
+            DropForeignKey("dbo.OrderService", "OrderId", "dbo.Order");
+            DropForeignKey("dbo.OrderProduct", "ProductId", "dbo.Product");
+            DropForeignKey("dbo.OrderProduct", "OrderId", "dbo.Order");
+            DropForeignKey("dbo.Order", "CustomerId", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserRole", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserLogin", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropForeignKey("dbo.IdentityUserClaim", "ApplicationUser_Id", "dbo.ApplicationUser");
+            DropIndex("dbo.OrderService", new[] { "ServiceId" });
+            DropIndex("dbo.OrderService", new[] { "OrderId" });
+            DropIndex("dbo.IdentityUserRole", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.IdentityUserRole", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserLogin", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaim", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.Order", new[] { "CustomerId" });
+            DropIndex("dbo.OrderProduct", new[] { "ProductId" });
+            DropIndex("dbo.OrderProduct", new[] { "OrderId" });
+            DropTable("dbo.IdentityRole");
+            DropTable("dbo.Service");
+            DropTable("dbo.OrderService");
+            DropTable("dbo.Product");
+            DropTable("dbo.IdentityUserRole");
+            DropTable("dbo.IdentityUserLogin");
+            DropTable("dbo.IdentityUserClaim");
+            DropTable("dbo.ApplicationUser");
+            DropTable("dbo.Order");
+            DropTable("dbo.OrderProduct");
+        }
+    }
+}
